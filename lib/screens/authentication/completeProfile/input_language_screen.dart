54e75d734c3d9079c3b6a14a83f59a1851ffc1a8
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2024-08-13 18:20:52 +0300

    Added side scrolling to language input screen

--- lib/screens/authentication/completeProfile/input_language_screen.dart
+++ lib/screens/authentication/completeProfile/input_language_screen.dart
@@ -26,6 +26,7 @@ class _InputLanguagesScreenState extends ConsumerState<InputLanguagesScreen> {
   Widget build(BuildContext context) {
     final userData = ref.watch(userDataProvider);
     final config = ref.watch(remoteConfigServiceProvider);
+    final ScrollController scrollController = ScrollController();
 
     return ConfigScreen(
       child: Column(
@@ -37,26 +38,55 @@ class _InputLanguagesScreenState extends ConsumerState<InputLanguagesScreen> {
           Text(config.get<String>('input-languages-disclaimer'), style: Theme.of(context).textTheme.labelSmall),
           const SizedBox(height: AppSizes.verticalSeparationPadding),
 
-          Wrap(
-          children: [
-            for (Language language in Language.values)
-              ChoiceButton(
-                text: config.get<String>(language.code),
-                isSelected: selectedLanguages.contains(language),
-                onSelected: (bool selected) {
-                  if (!selectedLanguages.contains(language)) {
-                      setState(() {
-                        selectedLanguages.add(language);
-                      });
-                  } else {
-                    setState(() {
-                      selectedLanguages.remove(language);
-                    });
-                  }
-                },
-              )
-          ],
-        ),
+          ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(
+              physics: const ClampingScrollPhysics(),
+            ),
+            child: RawScrollbar(
+              thumbVisibility: true,
+              trackVisibility: true,
+              thumbColor: Theme.of(context).colorScheme.primary.withOpacity(0.5),
+              trackColor: Theme.of(context).colorScheme.primary.withOpacity(0.1),
+              radius: const Radius.circular(10),
+              thickness: 3,
+              trackRadius: const Radius.circular(10),
+              controller: scrollController,
+              child: SingleChildScrollView(
+                controller: scrollController,
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: List.generate(3, (rowIndex) {
+                    int itemsPerRow = (Language.values.length / 3).ceil();
+                    int startIndex = rowIndex * itemsPerRow;
+                    int endIndex = (rowIndex + 1) * itemsPerRow;
+                    
+                    return Row(
+                      children: [
+                        for (Language language in Language.values.sublist(
+                          startIndex,
+                          endIndex.clamp(0, Language.values.length)
+                        ))
+                          ChoiceButton(
+                            text: config.get<String>(language.code),
+                            isSelected: selectedLanguages.contains(language),
+                            onSelected: (_) {
+                              setState(() {
+                                if (selectedLanguages.contains(language)) {
+                                  selectedLanguages.remove(language);
+                                } else {
+                                  selectedLanguages.add(language);
+                                }
+                              });
+                            },
+                          ),
+                      ],
+                    );
+                  }),
+                ),
+              ),
+            ),
+          ),
 
           
           const Spacer(),
