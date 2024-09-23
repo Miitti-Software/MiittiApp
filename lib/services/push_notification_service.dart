ab0282bd7c8b0810c95996c67d578ce036b5e54c
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2024-09-23 04:24:09 +0300

    Fixed Android notification icons

--- lib/services/push_notification_service.dart
+++ lib/services/push_notification_service.dart
@@ -92,7 +92,7 @@ class PushNotificationService {
   Future localNotiInit() async {
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
+        AndroidInitializationSettings('@drawable/ic_stat_name');
     final DarwinInitializationSettings initializationSettingsDarwin =
         DarwinInitializationSettings(
       onDidReceiveLocalNotification: (id, title, body, payload) {},
